fdcf8f9dca72e6f4f36dac3b019e278b59bd679a
langfei yang <185601452@qq.com>
2016-10-21 15:26:22 +0800

    music

--- app/src/main/java/com/yf/mymusic/fragment/MyFragment.java
+++ app/src/main/java/com/yf/mymusic/fragment/MyFragment.java
@@ -17,21 +17,14 @@ import com.yf.mymusic.R;
 public class MyFragment extends Fragment {
     private View view;
     private ImageView imageView;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         view=inflater.inflate(R.layout.ui_my_tab,container,false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.ui_my_tab,container,false);
         initView();
         return view;
     }
 
     private void initView() {
-        imageView=new ImageView(getActivity());
-        ViewGroup.LayoutParams lp=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        imageView.setLayoutParams(lp);
-        imageView.setImageResource(R.drawable.tz);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        imageView.setAdjustViewBounds(true);
+
     }
 }
